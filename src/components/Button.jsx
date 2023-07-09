fb4e342fcf8aa5e2b09f84b47a5a23f345cc7326
Roman <romanvoronin1994@gmail.com>
2023-07-09 18:27:46 +0300

    finish

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Button = ({ styles }) => {
   return (
-    <div type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles} rounded-[10px] cursor-pointer`}>
+    <div type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles} rounded-[10px] cursor-pointer transition-all ease-in-out delay-150 hover:scale-110`}>
       Get Started
     </div>
   )
